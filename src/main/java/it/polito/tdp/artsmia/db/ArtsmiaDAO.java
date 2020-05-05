afe912b25e03abba46666b1e3916dda37c83a186
Simona <Simona@LAPTOP-2JANVKQT.homenet.telecomitalia.it>
2020-05-05 16:48:14 +0200

    Completato es1 punti a e b

--- src/main/java/it/polito/tdp/artsmia/db/ArtsmiaDAO.java
+++ src/main/java/it/polito/tdp/artsmia/db/ArtsmiaDAO.java
@@ -6,15 +6,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import it.polito.tdp.artsmia.model.Adiacenza;
 import it.polito.tdp.artsmia.model.ArtObject;
 
 public class ArtsmiaDAO {
 
-	public List<ArtObject> listObjects() {
+	public void listObjects(Map<Integer, ArtObject> idMap) {
 		
 		String sql = "SELECT * from objects";
-		List<ArtObject> result = new ArrayList<>();
 		Connection conn = DBConnect.getConnection();
 
 		try {
@@ -22,20 +23,52 @@ public class ArtsmiaDAO {
 			ResultSet res = st.executeQuery();
 			while (res.next()) {
 
+				if(!idMap.containsKey(res.getInt("object_id"))) {
+				
 				ArtObject artObj = new ArtObject(res.getInt("object_id"), res.getString("classification"), res.getString("continent"), 
 						res.getString("country"), res.getInt("curator_approved"), res.getString("dated"), res.getString("department"), 
 						res.getString("medium"), res.getString("nationality"), res.getString("object_name"), res.getInt("restricted"), 
 						res.getString("rights_type"), res.getString("role"), res.getString("room"), res.getString("style"), res.getString("title"));
+				idMap.put(artObj.getId(),artObj);
 				
-				result.add(artObj);
+				}
+			}
+			conn.close();
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		}
+	}
+	
+	public List<Adiacenza> getAdiacenze(){
+		List<Adiacenza> result= new ArrayList<>();
+		String sql = "SELECT e1.object_id as id1, e2.object_id as id2, COUNT(*) as peso"
+				+ " FROM exhibition_objects AS e1 , exhibition_objects AS e2 \n" + 
+				"WHERE e1.exhibition_id=e2.exhibition_id \n" + 
+				"AND e2.object_id > e1.object_id \n" + 
+				"GROUP BY e1.object_id, e2.object_id";
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+
+				Adiacenza a= new Adiacenza(res.getInt("id1"),res.getInt("id2"),res.getInt("peso"));
+				result.add(a);
+				
+				
 			}
 			conn.close();
 			return result;
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return null;
+			
 		}
+		return null;
 	}
 	
 }
--- src/main/java/it/polito/tdp/artsmia/model/Model.java
+++ src/main/java/it/polito/tdp/artsmia/model/Model.java
@@ -1,5 +1,46 @@
 package it.polito.tdp.artsmia.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.artsmia.db.ArtsmiaDAO;
+
 public class Model {
 
+	private Graph<ArtObject, DefaultWeightedEdge> grafo;
+	private Map<Integer, ArtObject> idMap;
+	public Model() {
+		idMap= new HashMap<>();
+	}
+	public void creaGrafo() {
+		this.grafo= new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		//Aggiungere vertici
+		ArtsmiaDAO dao= new ArtsmiaDAO();
+		dao.listObjects(idMap);
+		Graphs.addAllVertices(this.grafo, idMap.values());
+		
+		// Aggiungere archi
+		
+		/*
+		 * Modo 2! oggetti adiacenti al primo MOLTO LUNGO
+		 *  SELECT e2.object_id, COUNT(*) FROM exhibition_objects AS e1 , exhibition_objects AS e2 
+		    WHERE e1.exhibition_id=e2.exhibition_id 
+		   AND e1.object_id= 8485 AND e2.object_id != e1.object_id  GROUP BY e2.object_id
+		
+		 */
+		
+		//MODO 3, SEMPRE IL MIGLIORE
+		for(Adiacenza a: dao.getAdiacenze()) {
+			if(a.getPeso()>0) {
+				Graphs.addEdge(this.grafo, idMap.get(a.getObj1()), idMap.get(a.getObj2()), a.getPeso());
+			}
+		}
+		
+		System.out.format("Grafo con %d vertici e %d archi",this.grafo.vertexSet().size(),this.grafo.edgeSet().size());
+	}
 }
